94baa0c1be064a582a12022743bf7ee1e2bac902
artem <artos2200@gmail.com>
2020-11-13 22:02:40 +0200

    add input_email

--- inputs.py
+++ inputs.py
@@ -2,27 +2,29 @@ import notifier
 import messages as constants
 
 
-def input_cmd():
+def input_value(message):
     while True:
-        cmd = input(constants.MSG_INPUT_CMD).strip().upper()
-        if cmd:
-            return cmd
+        value = input(message).strip().upper()
+        if value:
+            return value
         notifier.notify('input_some_data')
+
+
+def input_cmd():
+    return input_value(constants.MSG_INPUT_CMD)
 
 
 def input_name():
-    while True:
-        name = input(constants.MSG_INPUT_NAME).strip().upper()
-        if name:
-            return name
-        notifier.notify('input_some_data')
+    return input_value(constants.MSG_INPUT_NAME)
 
 
 def input_phone():
     while True:
-        phone = input(constants.MSG_INPUT_PHONE).strip().upper()
-        if phone:
-            if phone.isdigit():
-                return phone
-            notifier.notify('value_not_digit', phone)
-        notifier.notify('input_some_data')
+        phone = input_value(constants.MSG_INPUT_PHONE)
+        if phone.isdigit():
+            return phone
+        notifier.notify('value_not_digit', phone)
+
+
+def input_email():
+    return input_value(constants.MSG_INPUT_EMAIL)
