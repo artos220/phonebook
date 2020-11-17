0b0a687ed5125ba8b12ed0d6c503b2acf69f2a2d
artem <artos2200@gmail.com>
2020-11-17 23:13:19 +0200

    Contact add get

--- model.py
+++ model.py
@@ -4,34 +4,37 @@ class Contact:
         self.phone = phone
         self.email = email
 
-    def __str__(self):
-        return f'Contact(name={self.name}, phone={self.phone}, email={self.email})'
+    def props_get(self):
+        return [self.phone, self.email]
+
+    def name_get(self):
+        return self.name
 
     def __repr__(self):
         return f'Contact(name={self.name}, phone={self.phone}, email={self.email})'
 
 
 class Phonebook:
-    def __init__(self, phonebook):
+    def __init__(self, phonebook: {}):
         self.phonebook = phonebook
 
-    def contact_create(self, contact):
-        self.phonebook[contact.name] = [contact.phone, contact.email]
+    def contact_create(self, contact: Contact):
+        self.phonebook[contact.name_get()] = contact.props_get()
 
     def contact_get(self, name):
         return self.phonebook[name]
 
-    def contact_update(self, contact):
-        self.phonebook[contact.name] = [contact.phone, contact.email]
+    def contact_update(self, contact: Contact):
+        self.phonebook[contact.name_get()] = contact.props_get()
 
     def contact_delete(self, name):
         del self.phonebook[name]
 
-    def get(self):
-        return self.phonebook
+    def get(self) -> {}:
+        return self.phonebook.copy()
 
-    def set(self, data):
-        self.phonebook = data
+    def set(self, data: {}):
+        self.phonebook = data.copy()
 
     def contact_exists(self, name) -> bool:
         return name in self.phonebook
