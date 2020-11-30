558cd9038f15b0aee33024a31e9ee360ad891366
artem <artos2200@gmail.com>
2020-11-30 10:38:11 +0200

    add command line arguments usage

--- start_phonebook.py
+++ start_phonebook.py
@@ -1,27 +1,45 @@
 # Author: Artem Osmina
 # Description: Simple Phone Book
-# add package dumper for load/save phonebook
 
 import controller
 import notifier
 import inputs
+import cmd_argpars
 
-menu_action = {'C': controller.contact_create,
-               'R': controller.contact_read,
-               'U': controller.contact_update,
-               'D': controller.contact_delete,
+
+def not_found_cmd(cmd_):
+    return lambda: notifier.notify('not_found_cmd', cmd_)
+
+
+menu_action = {'C': lambda name='', phone='', email='': controller.contact_create(name, phone, email),
+               'R': lambda name='': controller.contact_read(name),
+               'U': lambda name='', phone='', email='': controller.contact_update(name, phone, email),
+               'D': lambda name='': controller.contact_delete(name),
                'A': controller.phonebook_read,
                'S': controller.phonebook_save,
                'H': controller.help_,
                'Q': controller.quit_,
                }
 
+args = cmd_argpars.get_args()
+
+#args.cmd = 'c'
+#args.name = 'ron3'
+#args.phone = 333444555
+
 try:
     notifier.notify('hello')
     controller.phonebook_load()
-    while True:
-        cmd = inputs.input_cmd()
-        menu_action.get(cmd, lambda: notifier.notify('not_found_cmd', cmd))()
+    if args.cmd:
+        menu_action.get(args.cmd.upper(), not_found_cmd(args.cmd.upper()))(args.name.upper(),
+                                                                           args.phone,
+                                                                           args.email,
+                                                                           )
+    else:
+        while True:
+            cmd = inputs.input_cmd()
+            menu_action.get(cmd, not_found_cmd(cmd))()
 finally:
     controller.phonebook_save()
     notifier.notify('exit')
+
