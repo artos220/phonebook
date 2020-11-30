8c24f4722bab51d54676ee14b87f43bb38756513
artem <artos2200@gmail.com>
2020-11-30 18:34:30 +0200

    bugfix

--- cmd_argpars.py
+++ cmd_argpars.py
@@ -5,7 +5,7 @@ parser.add_argument("-cmd", help="CRUD")
 parser.add_argument("-name", help="contact name")
 parser.add_argument("-phone", help="contact phone", default="")
 parser.add_argument("-email", help="contact email", default="")
-parser.add_argument("-h", help="start_phonebook.py -cmd=c -name=Test -phone=111000222", default="")
+parser.add_argument("-help", help="start_phonebook.py -cmd=c -name=Test -phone=111000222", default="")
 
 
 def parse_args(parser):
