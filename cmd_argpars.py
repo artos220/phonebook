36fa8f21d31ee9bfc6011b1e3737f5bd59a99763
artem <artos2200@gmail.com>
2020-11-30 10:42:41 +0200

    add help

--- cmd_argpars.py
+++ cmd_argpars.py
@@ -1,10 +1,11 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-cmd", help="")
-parser.add_argument("-name", help="")
-parser.add_argument("-phone", help="", default="")
-parser.add_argument("-email", help="", default="")
+parser.add_argument("-cmd", help="CRUD")
+parser.add_argument("-name", help="contact name")
+parser.add_argument("-phone", help="contact phone", default="")
+parser.add_argument("-email", help="contact email", default="")
+parser.add_argument("-h", help="start_phonebook.py -cmd=c -name=Test -phone=111000222", default="")
 
 
 def parse_args(parser):
