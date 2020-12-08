4d989b05220141950bb71dc1eb33b2b782662aa2
artem <artos2200@gmail.com>
2020-12-08 13:13:58 +0200

    decompose view polymorphism

---
+++ view/view.py
@@ -0,0 +1,8 @@
+from configs import config_reader as config
+
+if config.INPUT_TYPE.lower() == 'cmd':
+    from view.cmd_ import CmdView as View
+else:
+    from view.input_ import InputView as View
+
+View = View
