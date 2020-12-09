844197da789f70f8f834af2fcb90b94b90abf97a
artem <artos2200@gmail.com>
2020-12-09 12:47:12 +0200

    add cmd class and use view method for input type

--- start_phonebook.py
+++ start_phonebook.py
@@ -7,6 +7,17 @@ from view.view import View
 from configs import config_reader as config, messages as constants
 
 
+class Cmd:
+    key = 'Cmd'
+    msg_input = constants.MSG_INPUT_CMD
+
+    def get_input_type(self, name):
+        if name == 'msg_input':
+            return self.msg_input
+        else:
+            return self.key
+
+
 def not_found_cmd(cmd_):
     return lambda: notify('not_found_cmd', cmd_)
 
@@ -24,13 +35,12 @@ menu_action = {'C': controller.contact_create,
 try:
     notify('hello')
     controller.phonebook_load()
-    if config.INPUT_TYPE.lower() == 'cmd':
-        cmd = View('cmd').value
+    while True:
+        cmd = View(Cmd()).value
         menu_action.get(cmd, not_found_cmd(cmd))()
-    else:
-        while True:
-            cmd = View(constants.MSG_INPUT_CMD).value
-            menu_action.get(cmd, not_found_cmd(cmd))()
+        if config.INPUT_TYPE.lower() == 'cmd':
+            break
+
 finally:
     controller.phonebook_save()
     notify('exit')
